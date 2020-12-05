299e69f0f31f1ee3b7b5c59d474efc9afbe9e7af
AlexeyKutsenko <kutsenko.new@gmail.com>
2020-12-05 16:23:35 +0200

    Ticker component prices and quantities have been added

--- pa/src/components/Ticker.vue
+++ pa/src/components/Ticker.vue
@@ -4,7 +4,7 @@
   >
     <b-container fluid>
       <b-row>
-        <b-col cols="4">
+        <b-col cols="3">
           <b-card-text>
             {{ ticker.country }} / {{ ticker.sector }} / {{ ticker.industry }} / {{ ticker.symbol }}<br>
           </b-card-text>
@@ -19,26 +19,50 @@
             Last Updated: Unknown
           </div>
         </b-col>
-        <b-col cols="8">
-          <b-container fluid>
-            <b-row class="text-center">
+        <b-col cols="9">
+          <b-container>
+            <b-row class="text-center align-items-center">
+              <b-col
+                cols="3"
+              >
+                <div
+                  v-if="ticker.amount && ticker.price"
+                >
+                  <div>
+                    <b class="text-secondary">Amount: </b>
+                    <span class="text-warning">{{ ticker.amount }}</span>
+                  </div>
+                  <div>
+                    <b class="text-secondary">Price: </b>
+                    <span class="text-warning">{{ ticker.price }}</span>
+                  </div>
+                  <div>
+                    <b class="text-secondary">Cost: </b>
+                    <span class="text-warning">{{ ticker.cost }}</span>
+                  </div>
+                </div>
+              </b-col>
               <b-col
                 id="debt"
-                cols="4"
+                cols="3"
               >
                 <div
                   v-if="ticker.debt"
                 >
                   <div>
-                    <b>Debt to Equity:</b><br>{{ ticker.debt.debt_to_equity }}%<br>
-                    <b>Assets to Equity:</b><br>{{ ticker.debt.assets_to_equity }}<br>
+                    <b class="text-secondary">Debt to Equity: </b>
+                    <span class="text-warning">{{ ticker.debt.debt_to_equity }}%</span>
+                  </div>
+                  <div>
+                    <b class="text-secondary">Assets to Equity: </b>
+                    <span class="text-warning">{{ ticker.debt.assets_to_equity }}</span>
                   </div>
                 </div>
                 <div
                   v-else
                 >
-                  <b>Debt</b><br>
-                  Unknown<br>
+                  <b class="text-secondary">Debt</b>
+                  <span class="text-warning">Unknown</span>>
                   <b-icon
                     icon="question-circle"
                     scale="3"
@@ -48,19 +72,19 @@
               </b-col>
               <b-col
                 id="earnings"
-                cols="4"
+                cols="3"
               >
                 <div
                   v-if="ticker.annual_earnings_growth"
                 >
-                  <b>Annual Earnings Growth:</b><br>
-                  {{ ticker.annual_earnings_growth }}%
+                  <b class="text-secondary">Annual Earnings Growth: </b>
+                  <span class="text-warning">{{ ticker.annual_earnings_growth }}%</span>
                 </div>
                 <div
                   v-else
                 >
-                  <b>Annual Earnings Growth:</b><br>
-                  Unknown<br>
+                  <b class="text-secondary">Annual Earnings Growth: </b>
+                  <span class="text-warning">Unknown</span>>
                   <b-icon
                     icon="question-circle"
                     scale="3"
@@ -70,24 +94,24 @@
               </b-col>
               <b-col
                 id="returns"
-                cols="4"
+                cols="3"
               >
                 <div
                   v-if="ticker.returns_ratios"
                 >
                   <div>
-                    <b>Return on Assets:</b><br>
-                    {{ ticker.returns_ratios.roa }}%
+                    <b class="text-secondary">Return on Assets: </b>
+                    <span class="text-warning">{{ ticker.returns_ratios.roa }}%</span>
                   </div>
                   <div>
-                    <b>Return on Equity:</b><br>
-                    {{ ticker.returns_ratios.roe }}%
+                    <b class="text-secondary">Return on Equity: </b>
+                    <span class="text-warning">{{ ticker.returns_ratios.roe }}%</span>
                   </div>
                 </div>
                 <div
                   v-else
                 >
-                  Unknown<br>
+                  <span class="text-warning">Unknown</span>
                   <b-icon
                     icon="question-circle"
                     scale="3"
